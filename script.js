50e246a69d8bcb2555ea81e08c49a3d22e6e7011
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-03-31 22:59:51 +0000

    added some font styling and removed some text and list decoration

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 /*global $*/
+
+// allows the user to hit enter key on keyboard instead of having to press the submit button 
+var input = document.getElementById("wikiSearch");
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        $("#wikiSearchButton").click();
+    }
+});
+
 // Run JQuery
-$(document).ready(function(){
+$(document).ready(function() {
     // When search button is clicked run this code
-    $('#wikiSearchButton').click(function(){
+    $('#wikiSearchButton').click(function() {
         // retrieves the search phrase the user submitted
         var wikiSearch = $('#wikiSearch').val();
         // mediaWiki API url with user submitted search parameter, requesting JSON format and using the callback to get past CORS
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiSearch + "&format=json&callback=?";
-    
+
         $.ajax({
-            type:"GET",
-            url:url,
-            async:false,
-            dataType: "json", 
-            success: function(data){
-                // get heading ->  console.log(data[1][0]);
-                // get description ->  console.log(data[2][0]);
-                // get link ->  console.log(data[3][0]);
+            type: "GET",
+            url: url,
+            async: false,
+            dataType: "json",
+            success: function(data) {
+                // clears the search box of previous query
+                $('#dataDisplay').html('');
                 // loop through the search results and build up a list with the title being a link to the wikipedia page. Also displays a paragraph with a short descrition of the search result. 
-                for (var i=0; i<data[1].length; i++) {
-                    $('#dataDisplay').append("<li><a href= " + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
-                    
+                for (var i = 0; i < data[1].length; i++) {
+                    $('#dataDisplay').append("<li class='list'><a href= " + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
+
                 }
-                
-            }, 
-            error: function(errMessage){
+            },
+            error: function(errMessage) {
                 alert("Error" + errMessage);
             }
-        }) ;       
+        });
     });
 });
