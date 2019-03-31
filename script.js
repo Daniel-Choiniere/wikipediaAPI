6be34b01eeaa8bc3885fd2451d960c161ee1a906
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-03-31 13:19:22 +0000

    decided to use a AJAX request instead of using fetch

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-/*global fetch*/
+/*global today*/
 
+document.getElementById('wikiSearch').addEventListener('click', loadWikiSearch);
 
+// Load Daily Bitcoin Data
+function loadWikiSearch() {
+    
+    let xhr = new XMLHttpRequest();
 
-document.getElementById("wikiSearch").addEventListener("click", makeGETRequest);
-
-const ul = document.getElementById('results'); // Get the list where we will place our results
-var url = "https://en.wikipedia.org/w/api.php"; // Get 10 random results
-
-// ?action=query&list=search&srsearch=Abe%20Link&format=jsonfm
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-function makeGETRequest() {
-    fetch(url)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let searchResults = data.results;
-            return searchResults.map(function(author) {
-                let li = createNode('li'),
-                    img = createNode('img'),
-                    span = createNode('span');
-                img.src = author.picture.medium;
-                span.innerHTML = `${author.name.first} ${author.name.last}`;
-                append(li, img);
-                append(li, span);
-                append(ul, li);
-            })
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-}
+    xhr.open('GET', "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=JCQV2KWHYVU535I7", true);
+    
+    xhr.onload = function() {
+        if (this.status == 200) {
+            let wikiData = JSON.parse(this.responseText);
+            
+            // parsing through data to get what i need
+            let name = wikiData["Meta Data"]["3. Digital Currency Name"];
+        
+            // build the html to display the parsed data on webpage
+            let output = '<div class="user">' + 
+                        '<ul>' +
+                        '<li style="font-size: 30px">' + '<strong>' + name + '</strong>' + '</li>' +
+                        '<li>Daily Open: $' + open + '</li>' +
+                        '<li>Daily High: $' + high + '</li>' + 
+                        '<li>Daily Low: $' + low + '</li>' + 
+                        '<li>Daily Close: $' + close + '</li>' + 
+                        '<li>Daily Volume: $' + volume + '</li>' + 
+                        '<li>Daily Marketcap: $' + marketcap + '</li>' + 
+                        '<br>' +
+                        '<li><img src="https://images.pexels.com/photos/315788/pexels-photo-315788.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100&w=100"</li>' +
+                        '</ul>' +
+                         '</div>';
+            
+            document.getElementById('dataDisplay').innerHTML = output;
+        }    
+    };
+xhr.send();        
+}   
