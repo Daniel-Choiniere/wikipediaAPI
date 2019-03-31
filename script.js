bfed597d7455ccfc111911780b1e630512ffe629
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-03-31 19:30:28 +0000

    succesfully connecting to mediawiki API, and returning data

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-/*global today*/
-
-document.getElementById('wikiSearch').addEventListener('click', loadWikiSearch);
-
-// Load Daily Bitcoin Data
-function loadWikiSearch() {
+/*global $*/
+// Run JQuery
+$(document).ready(function(){
+    // When search button is clicked run this code
+    $('#wikiSearchButton').click(function(){
+        // retrieves the search phrase the user submitted
+        var wikiSearch = $('#wikiSearch').val();
+        // mediaWiki API url with user submitted search parameter, requesting JSON format and using the callback to get past CORS
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiSearch + "&format=json&callback=?";
     
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=JCQV2KWHYVU535I7", true);
-    
-    xhr.onload = function() {
-        if (this.status == 200) {
-            let wikiData = JSON.parse(this.responseText);
-            
-            // parsing through data to get what i need
-            let name = wikiData["Meta Data"]["3. Digital Currency Name"];
+        $.ajax({
+            type:"GET",
+            url:url,
+            async:false,
+            dataType: "json", 
+            success: function(data){
+                console.log(url);
+            }, 
+            error: function(errMessage){
+                alert("Error" + errMessage);
+            }
+        })        
         
-            // build the html to display the parsed data on webpage
-            let output = '<div class="user">' + 
-                        '<ul>' +
-                        '<li style="font-size: 30px">' + '<strong>' + name + '</strong>' + '</li>' +
-                        '<li>Daily Open: $' + open + '</li>' +
-                        '<li>Daily High: $' + high + '</li>' + 
-                        '<li>Daily Low: $' + low + '</li>' + 
-                        '<li>Daily Close: $' + close + '</li>' + 
-                        '<li>Daily Volume: $' + volume + '</li>' + 
-                        '<li>Daily Marketcap: $' + marketcap + '</li>' + 
-                        '<br>' +
-                        '<li><img src="https://images.pexels.com/photos/315788/pexels-photo-315788.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100&w=100"</li>' +
-                        '</ul>' +
-                         '</div>';
-            
-            document.getElementById('dataDisplay').innerHTML = output;
-        }    
-    };
-xhr.send();        
-}   
+    });
+});
